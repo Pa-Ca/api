f6f921a9b0b22f1ccf6be39b2fc4528f594eb3f1
Amin Arriaga <aminlorenzo.14@gmail.com>
2023-01-18 23:01:23 -0400

    Remove old JWT Authentication

--- pa-ca/src/main/java/com/paca/paca/exception/ExceptionHandler.java
+++ pa-ca/src/main/java/com/paca/paca/exception/ExceptionHandler.java
@@ -4,19 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @ControllerAdvice
 public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = {BadRequestException.class})
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = { BadRequestException.class })
     public ResponseEntity<Object> handleBadRequestException(BadRequestException e) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         ExceptionResponse response = new ExceptionResponse(e.getMessage(), status, ZonedDateTime.now());
         return new ResponseEntity<>(response, status);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = {NoContentException.class})
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = { NoContentException.class })
     public ResponseEntity<Object> handleBadNoContentException(NoContentException e) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         ExceptionResponse response = new ExceptionResponse(e.getMessage(), status, ZonedDateTime.now());
