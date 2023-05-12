73eaa2dddc17a7a8aac908e1d8a3f00ccfa751ef
Carlos Sivira <carlos.csivira@gmail.com>
2023-05-12 19:17:18 -0400

    (fix) removed deprecated methods in google auth service

--- pa-ca/src/main/java/com/paca/paca/auth/service/GoogleAuthenticationService.java
+++ pa-ca/src/main/java/com/paca/paca/auth/service/GoogleAuthenticationService.java
@@ -3,17 +3,16 @@ package com.paca.paca.auth.service;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.paca.paca.auth.dto.GoogleLoginRequestDTO;
-import com.paca.paca.auth.dto.LoginRequestDTO;
 import com.paca.paca.auth.dto.LoginResponseDTO;
-import com.paca.paca.auth.dto.SignupRequestDTO;
 import com.paca.paca.auth.utils.AuthUtils;
-import com.paca.paca.exception.exceptions.BadRequestException;
-import com.paca.paca.exception.exceptions.ConflictException;
-import com.paca.paca.exception.exceptions.ForbiddenException;
-import com.paca.paca.exception.exceptions.UnprocessableException;
+import com.paca.paca.exception.exceptions.*;
+import com.paca.paca.statics.AuthProvider;
+import com.paca.paca.statics.UserRole;
+import com.paca.paca.user.model.Role;
 import com.paca.paca.user.model.User;
+import com.paca.paca.user.repository.RoleRepository;
 import com.paca.paca.user.repository.UserRepository;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,6 +26,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 
 import java.util.Collections;
+import java.util.Optional;
 
 @Service
 @Getter
@@ -36,6 +36,7 @@ public class GoogleAuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationService authenticationService;
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final JwtService jwtService;
     @Value("${google.client.id}")
     private String CLIENT_ID;
@@ -43,7 +44,7 @@ public class GoogleAuthenticationService {
     private Payload verifyToken(String token)
             throws BadRequestException {
         HttpTransport transport = new NetHttpTransport();
-        JsonFactory jsonFactory = new JacksonFactory();
+        JsonFactory jsonFactory = new GsonFactory();
 
         if (token == null) {
             throw new BadRequestException("No Google Access token was provided", 100);
@@ -68,13 +69,40 @@ public class GoogleAuthenticationService {
     public LoginResponseDTO signup(GoogleLoginRequestDTO request)
             throws BadRequestException, UnprocessableException, ConflictException, ForbiddenException {
         Payload payload = verifyToken(request.getToken());
-        SignupRequestDTO dto = SignupRequestDTO.builder()
+
+        // Email Validation
+        if (userRepository.existsByEmail(payload.getEmail())) {
+            throw new ConflictException("User already exists", 1);
+        }
+
+        // Role Validation
+        AuthUtils.validateRole(request.getRole());
+        Optional<Role> role = roleRepository.findByName(UserRole.valueOf(request.getRole()));
+        if (role.isEmpty()) {
+            throw new NoContentException("Role " + request.getRole() + " does not exists");
+        }
+
+        // Create and save new User
+        User user = User.builder()
                 .email(payload.getEmail())
                 .password(AuthUtils.randomPasswordGenerator(64))
-                .role(request.getRole())
+                .role(role.get())
+                .verified(false)
+                .loggedIn(false)
+                .provider(AuthProvider.google.name())
+                .provider_id(payload.getSubject())
+                .build();
+        user = userRepository.save(user);
+
+        // Generate JWT token and Response
+        LoginResponseDTO response = LoginResponseDTO.builder()
+                .token(jwtService.generateToken(user, JwtService.TokenType.TOKEN))
+                .refresh(jwtService.generateToken(user, JwtService.TokenType.REFRESH))
+                .id(user.getId())
+                .role(user.getRole().getName().name())
                 .build();
 
-        return authenticationService.signup(dto);
+        return response;
     }
 
     public LoginResponseDTO login(GoogleLoginRequestDTO request)
