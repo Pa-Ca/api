af6fdb159040f83283b938bf154d8d222cc9893a
Eduardo Lopez <eduardo98m@gmail.com>
2023-07-03 15:12:55 -0400

    minor changes

--- pa-ca/src/test/java/com/paca/paca/sale/SaleServiceTest.java
+++ pa-ca/src/test/java/com/paca/paca/sale/SaleServiceTest.java
@@ -34,7 +34,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 
 import java.util.ArrayList;
 import java.util.List;
