bc99cfb33f6d2dac59dc93c6bdc8aa2b4b7a7a4d
eduardo98m <eduardo98m@gmail.com>
2023-08-07 12:39:01 -0400

    Completed Tests

--- pa-ca/src/test/java/com/paca/paca/branch/TableServiceTest.java
+++ pa-ca/src/test/java/com/paca/paca/branch/TableServiceTest.java
@@ -1,5 +1,4 @@
 package com.paca.paca.branch;
-import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
@@ -7,12 +6,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.Assert;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.InjectMocks;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,12 +22,8 @@ import com.paca.paca.branch.repository.BranchRepository;
 import com.paca.paca.branch.repository.TableRepository;
 import com.paca.paca.branch.service.TableService;
 import com.paca.paca.branch.utils.TableMapper;
-import com.paca.paca.reservation.model.Reservation;
-import com.paca.paca.sale.dto.SaleDTO;
-import com.paca.paca.sale.dto.SaleInfoDTO;
-import com.paca.paca.sale.model.Sale;
-import com.paca.paca.sale.model.SaleProduct;
-import com.paca.paca.sale.model.Tax;
+import com.paca.paca.exception.exceptions.ConflictException;
+import com.paca.paca.exception.exceptions.NoContentException;
 import com.paca.paca.utils.TestUtils;
 
 @ExtendWith(MockitoExtension.class)
@@ -68,6 +61,38 @@ public class TableServiceTest {
         assertThat(response).isNotNull();
     }
 
+    @Test
+    void shouldGetNoContentExceptionDueToBranchNoExistingInSave(){
+        TableDTO  tableDTO = utils.createTableDTO(null);
+        long branchId = tableDTO.getBranchId();
+
+        when(branchRepository.findById(anyLong())).thenReturn(Optional.empty()); 
+
+        try {
+            tableService.save(tableDTO);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof NoContentException);
+            Assert.assertEquals(e.getMessage(), "Branch with id " + branchId + " does not exists");
+            Assert.assertEquals(((NoContentException) e).getCode(), (Integer) 20);
+        }
+    }
+
+    @Test 
+    void shouldGetConflictExceptionDueToTableAlreadyExistingInSave(){
+        TableDTO  tableDTO = utils.createTableDTO(null);
+        Branch branch = utils.createBranch(null);
+
+        when(branchRepository.findById(anyLong())).thenReturn(Optional.of(branch));
+
+        try {
+            tableService.save(tableDTO);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof ConflictException);
+            Assert.assertEquals(e.getMessage(), "Table with id " + tableDTO.getId() + " already exists");
+            Assert.assertEquals(((ConflictException) e).getCode(), (Integer) 55);
+        }
+    }
+
 
     @Test
     void shouldUpdate(){
@@ -86,6 +111,57 @@ public class TableServiceTest {
     }
 
     @Test
+    void shouldGetNoContentExceptionDueToTableNotExisistingInUpdate(){
+        TableDTO  tableDTO = utils.createTableDTO(null);
+        long tableId = tableDTO.getId();
+
+        when(tableRepository.findById(anyLong())).thenReturn(Optional.empty()); 
+
+        try {
+            tableService.update(tableId ,tableDTO);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof NoContentException);
+            Assert.assertEquals(e.getMessage(), "Table with id " + tableId + " does not exists");
+            Assert.assertEquals(((NoContentException) e).getCode(), (Integer) 49);
+        }
+    }
+
+    @Test
+    void shouldGetNoContentExceptionDueToBranchNotExisistingInUpdate(){
+        TableDTO  tableDTO = utils.createTableDTO(null);
+        long tableId = tableDTO.getId();
+
+        when(tableRepository.findById(anyLong())).thenReturn(Optional.empty()); 
+
+        try {
+            tableService.update(tableId ,tableDTO);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof NoContentException);
+            Assert.assertEquals(e.getMessage(), "Table with id " + tableId + " does not exists");
+            Assert.assertEquals(((NoContentException) e).getCode(), (Integer) 49);
+        }
+    }
+
+    @Test
+    void shouldGetConflictExceptionDueToTableAlreadyExistingInUpdate(){
+        TableDTO  tableDTO = utils.createTableDTO(null);
+        Table table = utils.createTable(null);
+        Branch branch = utils.createBranch(null);
+
+        when(branchRepository.findById(anyLong())).thenReturn(Optional.of(branch));
+        when(tableRepository.findById(anyLong())).thenReturn(Optional.of(table));
+        when(tableRepository.existsByBranchIdAndNameAndDeletedFalse(anyLong(), any())).thenReturn(true);
+
+        try {
+            tableService.update(1L ,tableDTO);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof ConflictException);
+            Assert.assertEquals(e.getMessage(), "Table with name " + tableDTO.getName() + " already exists");
+            Assert.assertEquals(((ConflictException) e).getCode(), (Integer) 55);
+        }
+    }
+
+    @Test
     void shouldDelete(){
         Table table = utils.createTable(null);
 
@@ -95,6 +171,19 @@ public class TableServiceTest {
         // Yes the method to test is the save method not the delete method.
         // This is beacause 
         verify(tableRepository, times(1)).save(table);
+    }
+
+    @Test
+    void shouldGetNoContentExceptionDueToTableNotExisistingInDelete(){
+        when(tableRepository.findById(anyLong())).thenReturn(Optional.empty()); 
+
+        try {
+            tableService.delete(1L);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof NoContentException);
+            Assert.assertEquals(e.getMessage(), "Table with id " + 1L + " does not exists");
+            Assert.assertEquals(((NoContentException) e).getCode(), (Integer) 49);
+        }
     }
 
 }
