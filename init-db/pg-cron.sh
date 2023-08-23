bf05756652d5533bd95e8f7d71d5db5934011dfb
JRBarreraM <mrjrbarreram@gmail.com>
2023-08-23 10:47:46 -0400

    pg_cron works with springboot tests

--- init-db/pg-cron.sh
+++ init-db/pg-cron.sh
@@ -3,14 +3,10 @@
 # use the same database as the one from the environment variables
 dbname="$POSTGRES_DB"
 
-# create custom config
-customconf=/var/lib/postgresql/data/custom-conf.conf
-echo "" > $customconf
-echo "shared_preload_libraries = 'pg_cron'" >> $customconf
-echo "cron.database_name = '$dbname'" >> $customconf
-chown postgres $customconf
-chgrp postgres $customconf
-
 # include custom config from the main config
+# cat /var/lib/postgresql/data/postgresql.conf
 conf=/var/lib/postgresql/data/postgresql.conf
-echo "include = '$customconf'" >> $conf
+echo "" >> $conf
+echo "# pg_cron extension" >> $conf
+echo "shared_preload_libraries = 'pg_cron'" >> $conf
+echo "cron.database_name = '$dbname'" >> $conf
--- pa-ca/src/test/java/com/paca/paca/PacaTest.java
+++ pa-ca/src/test/java/com/paca/paca/PacaTest.java
@@ -3,6 +3,7 @@ package com.paca.paca;
 import io.github.cdimascio.dotenv.Dotenv;
 
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.DynamicPropertyRegistry;
 
@@ -10,10 +11,15 @@ public abstract class PacaTest {
     public static final PostgreSQLContainer<?> POSTGRES_SQL_CONTAINER;
 
     static {
-        POSTGRES_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:15.1");
-        POSTGRES_SQL_CONTAINER.start();
-
         Dotenv dotenv = Dotenv.load();
+
+        DockerImageName myImage = DockerImageName.parse("jrbarreram/postgres_extensions:v1.0");
+        myImage = myImage.asCompatibleSubstituteFor("postgres");
+        POSTGRES_SQL_CONTAINER = new PostgreSQLContainer<>(myImage);
+        POSTGRES_SQL_CONTAINER.withDatabaseName(dotenv.get("SPRING_DATASOURCE_DATABASE_NAME"))
+        .withUsername(dotenv.get("SPRING_DATASOURCE_USERNAME"))
+        .withPassword(dotenv.get("SPRING_DATASOURCE_PASSWORD"))
+        .start();
 
         System.setProperty("auth.secret.key", dotenv.get("AUTH_SECRET_KEY"));
         System.setProperty("auth.expiration.token", dotenv.get("AUTH_TOKEN_EXPIRATION"));
